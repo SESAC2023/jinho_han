9e62317f5638f73080f00ccf89c24b2fca9fad2b
JinHo Han <52950175+jinho2070@users.noreply.github.com>
2023-06-21 18:01:03 +0900

    Update 11718_그대로 출력하기.py

--- BOJ/단계별로 풀어보기/5.문자열/11718_그대로 출력하기.py
+++ BOJ/단계별로 풀어보기/5.문자열/11718_그대로 출력하기.py
@@ -1,5 +1,5 @@
 while True:
     try:
         print(input())
-    except EOFError:    # EOF: End Of File. 즉 입력값이 더 이상 없는 상황을 뜻함
+    except EOFError:    # EOF: End Of File. 즉 입력값이 더 이상 없는 상황을 뜻함 => 그런데 이때 EOFError를 사용하기 위해서는 sys.stdin.readline()을 사용해서는 안됨. input()을 사용해야 함.
         break           # 입력값이 더 이상 없으면 멈춰라.
