d40bb26c8ef4bf88cfb245a333040557ea0981af
JinHo Han <52950175+jinho2070@users.noreply.github.com>
2023-06-21 16:36:11 +0900

    Create 5622_다이얼.py

---
+++ BOJ/단계별로 풀어보기/5.문자열/5622_다이얼.py
@@ -0,0 +1,23 @@
+data = input()
+data = [i for i in data]
+
+temp = []
+for j in data:
+    if j in ['A', 'B', 'C']:
+        temp.append(3)
+    elif j in ['D', 'E', 'F']:
+        temp.append(4)
+    elif j in ['G', 'H', 'I']:
+        temp.append(5)
+    elif j in ['J', 'K', 'L']:
+        temp.append(6)
+    elif j in ['M', 'N', 'O']:
+        temp.append(7)
+    elif j in ['P', 'Q', 'R', 'S']:
+        temp.append(8)
+    elif j in ['T', 'U', 'V']:
+        temp.append(9)
+    elif j in ['W', 'X', 'Y', 'Z']:
+        temp.append(10)
+
+print(sum(temp))
