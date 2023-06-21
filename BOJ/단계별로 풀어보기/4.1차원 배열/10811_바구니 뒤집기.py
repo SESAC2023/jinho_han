35dbd7a324224329496ac5f1c8cf459dad723b34
JinHo Han <52950175+jinho2070@users.noreply.github.com>
2023-06-21 21:02:23 +0900

    Update 10811_바구니 뒤집기.py

--- BOJ/단계별로 풀어보기/4.1차원 배열/10811_바구니 뒤집기.py
+++ BOJ/단계별로 풀어보기/4.1차원 배열/10811_바구니 뒤집기.py
@@ -11,3 +11,36 @@ for j in range(M):
 
 for k in answer:
     print(k, end=' ')             # 리스트 말고 띄어쓰기 된 숫자로 출력
+
+
+
+''' 1번 풀이
+n, m = map(int, input().split())
+arr = [i for i in range(1, n+1)]
+
+for i in range(m);
+    left, right = map(int, input().split())
+    left -= 1
+    arr[left:right] = reversed(arr[left:right])
+
+for x in arr:
+    print(x, end=' ')
+'''
+
+
+''' 2번 풀이
+n, m = map(int, input().split())
+arr = [i for i in range(1, n + 1)]
+
+for i in range(m):
+    left, right = map(int, input().split())
+    left -= 1
+    temp = []
+    for j in range(left, right):
+        temp.append(arr[j])
+    for j in range(left, right):
+        arr[j] = temp.pop()
+
+for x in arr:
+    print(x, end=" ")
+'''
