e4cdb65c4db825f4455f2b4bf987a2b68f174981
JinHo Han <52950175+jinho2070@users.noreply.github.com>
2023-06-21 15:13:01 +0900

    Update 11654_아스키 코드.py

--- BOJ/단계별로 풀어보기/5.문자열/11654_아스키 코드.py
+++ BOJ/단계별로 풀어보기/5.문자열/11654_아스키 코드.py
@@ -1,5 +1,5 @@
 data = input()
 if type(data) == str:
-    print(ord(data))
+    print(ord(data))       # ord(문자열): ord는 문자를 입력값으로 받으며, 문자의 아스키코드를 출력해준다. (소문자, 대문자 모두)
 else:
-    print(chr(int(data)))
+    print(chr(int(data)))  # chr(정수): chr은 정수를 입력값으로 받으며, 숫자의 아스키코드를 출력해준다.
