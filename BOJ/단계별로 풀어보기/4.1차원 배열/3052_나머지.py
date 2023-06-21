48bef94f29b1687c89cb17d51d12c25fc3cc4631
JinHo Han <52950175+jinho2070@users.noreply.github.com>
2023-06-21 20:53:08 +0900

    Update 3052_나머지.py

--- BOJ/단계별로 풀어보기/4.1차원 배열/3052_나머지.py
+++ BOJ/단계별로 풀어보기/4.1차원 배열/3052_나머지.py
@@ -6,3 +6,25 @@ for i in range(10):
 
 answer = len(list(set(answer)))
 print(answer)
+
+
+'''
+- 1번 솔루션
+arr = []
+
+for i in range(10):
+    x = int(input())
+    arr.append(x % 42)
+
+print(len(set(arr)))
+
+
+- 2번 솔루션
+data = set()
+
+for i in range(10):
+    x = int(input())
+    data.add(x % 42)
+
+print(len(data))
+'''
