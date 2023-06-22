533db233b263689ca97624d2428c30ad58292491
JinHo Han <52950175+jinho2070@users.noreply.github.com>
2023-06-22 20:47:23 +0900

    Update 1157_단어공부.py

--- BOJ/단계별로 풀어보기/6.심화 1/1157_단어공부.py
+++ BOJ/단계별로 풀어보기/6.심화 1/1157_단어공부.py
@@ -13,3 +13,36 @@ if temp_vals.count(temp_max) >=2:  # 최대값(temp_max)이 2개 이상이면,
     print('?')                     # 물음표(?) 반환
 else:                              # 최대값(temp_max)이 1개만 있다면
     print([k for k, v in temp.items() if v == temp_max][0])  # 최대값에 맞는 key(알파벳)을 반환
+
+
+'''
+counter = dict()
+data = input()
+
+# 전형적인 카운터(counter) 구현(중요)
+for x in data:
+    x = x.upper() # 무조건 대문자로 통일
+    if x not in counter:
+        counter[x] = 1
+    else:
+        counter[x] += 1
+
+# 가장 많이 사용된 알파벳을 찾기
+max_cnt = 0
+max_char = "a"
+for (key, value) in counter.items():
+    if value > max_cnt:
+        max_cnt = value
+        max_char = key
+
+# 가장 많이 사용된 알파벳이 2개 이상인지 확인
+cnt = 0
+for (key, value) in counter.items():
+    if value == max_cnt:
+        cnt += 1
+
+if cnt >= 2:
+    print("?")
+else:
+    print(max_char)
+'''
