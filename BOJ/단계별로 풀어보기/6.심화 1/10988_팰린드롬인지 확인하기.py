ad2898429acfe6f6eb168f33b955ef35d1bf6fc3
JinHo Han <52950175+jinho2070@users.noreply.github.com>
2023-06-22 20:39:16 +0900

    Update 10988_팰린드롬인지 확인하기.py

--- BOJ/단계별로 풀어보기/6.심화 1/10988_팰린드롬인지 확인하기.py
+++ BOJ/단계별로 풀어보기/6.심화 1/10988_팰린드롬인지 확인하기.py
@@ -5,3 +5,44 @@ if N == N_back:
     print(1)
 else:
     print(0)
+
+
+"""
+data = input()
+reversed_data = reversed(data)
+
+if data == reversed_data:
+    print(1)
+else:
+    print(0)
+"""
+
+"""
+def pelindrome(data):
+    for i in range(len(data)):
+        if data[i] != data[len(data) - i - 1]:
+            return False
+    return True
+
+data = input()
+
+if pelindrome(data):
+    print(1)
+else:
+    print(0)
+"""
+
+'''
+data = input()
+reversed_data = data[::-1] # 이 코드는 많이 쓰임
+# data[2:5] == [data[2], data[3], data[4]]
+# data[2:5:-1] == [data[4], data[3], data[2]]
+# data[:] == data
+# data[::-1] == reversed(data)
+# (start, end, step)
+
+if data == reversed_data:
+    print(1)
+else:
+    print(0)
+'''
